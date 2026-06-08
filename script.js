ef22dd9ad238d91acf8b446d25003bd90ed827d7
isabellelee-coder <isabelle.lee.pms@gmail.com>
2026-06-07 22:17:57 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function saveData(level) {
+    const input = document.getElementById("dataInput");
+    localStorage.setItem(level, input.value);
+    alert("Saved!");
+}
+
+function loadData(level) {
+    const display = document.getElementById("savedData");
+    const saved = localStorage.getItem(level);
+
+    if (saved) {
+        display.textContent = saved;
+    } else {
+        display.textContent = "No information has been added yet.";
+    }
+}
